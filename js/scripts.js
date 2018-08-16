c00392187490ddcdb3c70c08954a1aeff5110edb
SkyeN <skye@dames.es>
2018-08-16 11:37:05 -0700

    remove redundant code

--- js/scripts.js
+++ js/scripts.js
@@ -4,31 +4,33 @@ var romanNumber = {
   "I": 1,
   "IV": 4,
   "V": 5,
-  "IX":9,
+  "IX": 9,
   "X": 10,
   "XL": 40,
   "L": 50,
   "XC": 90,
   "C": 100,
-  "CD":400,
+  "CD": 400,
   "D": 500,
-  "CM":900,
+  "CM": 900,
   "M": 1000,
 }
 
 var romanFindMod = function (input) {
-  
-  var result = "";
-  for (var i of Object.keys(romanNumber).reverse()) {
-    var quotient = Math.floor(input / (romanNumber)[i]);
-    console.log(i);
-    console.log("this is the quotient");
-    console.log(quotient);
-    input -= quotient * (romanNumber)[i];
-    for (var j = 0; j< quotient;j ++) {
-      result += i;
+  if (input < 1 || input > 3999) {
+    alert("Please enter a number between 1 and 3999!");
+  }
+  else {
+    var result = "";
+    for (var i of Object.keys(romanNumber).reverse()) {
+      var quotient = Math.floor(input / romanNumber[i]);
+      input -= quotient * (romanNumber)[i];
+      for (var j = 0; j < quotient; j++) {
+        result += i;
+      }
     }
   }
+
   return result;
 }
 
@@ -37,10 +39,6 @@ $(document).ready(function () {
   $("form#input").submit(function (event) {
     event.preventDefault();
     var input = parseInt($("#inputNumber").val());
-    console.log(input);
-    console.log(romanFindMod(input));
-
-    // var result = RomanConverter(input);
     $("#resultNumber").empty();
     $("#resultNumber").append(romanFindMod(input));
   })
